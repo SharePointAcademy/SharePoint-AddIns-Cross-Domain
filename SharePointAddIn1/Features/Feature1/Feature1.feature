60f8ba73367bf458aa4682384015a43477e491d5
Saulo Oliveira <sauslash@hotmail.com>
2020-08-30 19:18:17 -0300

    initial commit

---
+++ SharePointAddIn1/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="182bab21-59b3-4e15-88f6-11ebc51f3099" description="SharePoint Add-in Feature" featureId="182bab21-59b3-4e15-88f6-11ebc51f3099" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SharePointAddIn1 Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="bb4ed807-4c20-4a77-ac47-3d80d50ca905" />
+    <projectItemReference itemId="53142bfb-5860-435f-a40e-e929d9158771" />
+    <projectItemReference itemId="7ecbf952-5e0e-4efc-ba20-1a06f4015201" />
+    <projectItemReference itemId="8600698a-0f94-4210-b358-a25eb3971a89" />
+  </projectItems>
+</feature>
---
+++ SharePointAddIn1/Scripts/App.js
@@ -0,0 +1,100 @@
+﻿'use strict';
+
+var hostweburl;
+var appweburl;
+
+$(document).ready(function () {
+    hostweburl =
+        decodeURIComponent(
+            getQueryStringParameter("SPHostUrl")
+        );
+    appweburl =
+        decodeURIComponent(
+            getQueryStringParameter("SPAppWebUrl")
+        );
+
+    console.log("hostweburl", hostweburl);
+    console.log("appweburl", appweburl);
+
+    var scriptbase = hostweburl + "/_layouts/15/";
+    $.getScript(scriptbase + "SP.RequestExecutor.js", execCrossDomainRequest);
+});
+
+function execCrossDomainRequest() {
+    var executor = new SP.RequestExecutor(appweburl);
+
+    executor.executeAsync(
+        {
+            url: appweburl + "/_api/SP.AppContextSite(@target)/web/lists/getbytitle('Product')/items?@target='" + hostweburl + "'&$top=10&$orderby=ID desc",
+            method: "GET",
+            headers: { "Accept": "application/json; odata=verbose" },
+            success: successHandler,
+            error: errorHandler
+        }
+    );
+}
+
+function successHandler(data) {
+    var jsonObject = JSON.parse(data.body);
+    var productsHTML = "";
+    var results = jsonObject.d.results;
+    for (var i = 0; i < results.length; i++) {
+        productsHTML = productsHTML + "<div>" + results[i].name + "</div><br>";
+    }
+
+    $('#products').html(productsHTML);
+
+}
+
+function errorHandler(data, errorCode, errorMessage) {
+    document.getElementById("noticias").innerText =
+        "Could not complete cross-domain call: " + errorMessage;
+}
+
+
+function getQueryStringParameter(paramToRetrieve) {
+    var params =
+        document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] === paramToRetrieve)
+            return singleParam[1];
+    }
+}
+
+function adicionarProduto() {
+    //insere um produto
+    var data = {
+        __metadata: { 'type': 'SP.Data.ProductListItem' },
+        name: 'a-SampleData',
+        stock: 10,
+        price: 2.60
+    };
+
+    var urlSiteDev = appweburl + "/_api/SP.AppContextSite(@target)/web/lists/getbytitle('Product')/items?@target='" + hostweburl + "'";
+    addNewItem(urlSiteDev, data);
+}
+
+function addNewItem(restUrl, data) {
+    $.ajax({
+        url: restUrl,
+        type: "POST",
+        data: JSON.stringify(data),
+        headers: {
+            "Accept": "application/json;odata=verbose",
+            "Content-Type": "application/json;odata=verbose",
+            "X-RequestDigest": $("#__REQUESTDIGEST").val(),
+            "X-HTTP-Method": "POST"
+        },
+        success: function (data) {
+            alert("Produto inserido com sucesso");
+            execCrossDomainRequest();
+
+        },
+        error: function (error) {
+            console.log(JSON.stringify(error));
+        }
+    });
+}
+
